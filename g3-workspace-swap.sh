62d638597d77676bf2d75a4bf68a28dd25570bfe
Victor Arribas <v.arribas.urjc@gmail.com>
2019-11-13 23:37:09 +0100

    settings migrated

--- g3-workspace-swap.sh
+++ g3-workspace-swap.sh
@@ -1,14 +1,21 @@
 #!/bin/bash
 
+# http://gregcor.com/2011/05/07/fix-dual-monitors-in-gnome-3-aka-my-workspaces-are-broken/
 # --------------------------------------------------------- #
 # Simple Gnome 3 script to allowing switching dual monitor  #
 # workspace switching on and off - jamesbos@gmail.com       #
+# Round 2: v.arribas.urjc@gmail.com
 # --------------------------------------------------------- #
 
-value=$(gsettings get org.gnome.shell.overrides workspaces-only-on-primary)
+# Note [2019.11.13]
+# settings migrated from `org.gnome.shell.overrides`
+# to `org.gnome.mutter` since Gonome 3.30.0
+# See: https://gitlab.gnome.org/GNOME/gnome-shell/issues/612
 
-SETTRUE='gsettings set org.gnome.shell.overrides workspaces-only-on-primary true'
-SETFALSE='gsettings set org.gnome.shell.overrides workspaces-only-on-primary false'
+
+value=$(gsettings get org.gnome.mutter workspaces-only-on-primary)
+SETTRUE='gsettings set org.gnome.mutter workspaces-only-on-primary true'
+SETFALSE='gsettings set org.gnome.mutter workspaces-only-on-primary false'
 
 if [ $value == 'true' ]
 then
